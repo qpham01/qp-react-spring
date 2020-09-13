5cd5f3efa573074c022df86a4609fd111caf33fd
= <quoc_pham@msn.com>
2020-09-13 13:15:44 -0500

    Created REST API

---
+++ cardatabase/src/main/java/com/qpham/cardatabase/web/CarController.java
@@ -0,0 +1,20 @@
+package com.qpham.cardatabase.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qpham.cardatabase.domain.Car;
+import com.qpham.cardatabase.domain.CarRepository;
+
+@RestController
+public class CarController {
+    
+    @Autowired
+    private CarRepository repository;
+    
+    @RequestMapping("/cars")
+    public Iterable<Car> getCars() {
+        return repository.findAll();
+    }
+}
